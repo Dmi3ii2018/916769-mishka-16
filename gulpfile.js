fbcd985ee3d568dbc97b34a0f0724542d09f5363
Dmitrii Alekseev <dalekseev2017@gmail.com>
2019-05-27 01:50:41 +0500

    Оптимизировал картинки

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@ var gulp = require("gulp");
 var plumber = require("gulp-plumber");
 var sourcemap = require("gulp-sourcemaps");
 var rename = require("gulp-rename");
+var imagemin = require("gulp-imagemin");
 
 var sass = require("gulp-sass");
 var postcss = require("gulp-postcss");
@@ -24,6 +25,16 @@ gulp.task("css", function () {
     .pipe(sourcemap.write("."))
     .pipe(gulp.dest("source/css"))
     .pipe(server.stream());
+});
+
+gulp.task("images", function () {
+  return gulp.src("source/img/**/*.{png,jpg,svg}")
+    .pipe(imagemin([
+      imagemin.optipng({ optimizationLevel: 3 }),
+      imagemin.jpegtran({ progressive: true }),
+      imagemin.svgo()
+    ]))
+    .pipe(gulp.dest("source/img"))
 });
 
 gulp.task("server", function () {
