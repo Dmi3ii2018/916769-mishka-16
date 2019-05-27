cfd6d7d0200ce49fa0fd055e7de8c2e81f2ef781
Dmitrii Alekseev <dalekseev2017@gmail.com>
2019-05-27 12:26:19 +0500

    Поменял source на build

--- gulpfile.js
+++ gulpfile.js
@@ -83,7 +83,7 @@ gulp.task("clean", function () {
 
 gulp.task("server", function () {
   server.init({
-    server: "source/",
+    server: "build/",
     notify: false,
     open: true,
     cors: true,
